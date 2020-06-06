9d4c1a2b00154a65896cda47f314eac2f1ab3ae5
Thiru <thirumaran.thurarajah@gmail.com>
2020-06-07 08:08:57 +1000

    - Implemented product options CRUD functionality - Refactored handler code to be more readable and general - Fixed up database with PK and FK relationship - Removed unnecessary controller section

--- cmd/app/model/productOption.go
+++ cmd/app/model/productOption.go
@@ -2,10 +2,10 @@ package model
 
 // Product is the basis for product
 type ProductOption struct {
-	ID          string `gorm:"column:Id;type:varchar;primary_key" json:"Id"`
-	ProductID   string `gorm:"column:ProductId;type:varchar" json:"ProductId"`
-	Name        string `gorm:"column:Name;type:varchar" json:"Name"`
-	Description string `gorm:"column:Description;type:varchar" json:"Description"`
+	ID          string `gorm:"column:Id;type:varchar;primary_key" json:"Id" query:"id"`
+	ProductID   string `gorm:"column:ProductId;type:varchar" json:"ProductId" query:"ProductId"`
+	Name        string `gorm:"column:Name;type:varchar" json:"Name" query:"Name"`
+	Description string `gorm:"column:Description;type:varchar" json:"Description" query:"Description"`
 }
 
 type ProductOptionList struct {
--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	r := router.New()
+
 	v1 := r.Group("/products")
+
 	handler.NewProductHandler(storage.New()).Register(v1)
 	r.Logger.Fatal(r.Start("127.0.0.1:8585"))
 }
